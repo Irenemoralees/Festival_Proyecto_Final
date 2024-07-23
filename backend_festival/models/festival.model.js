8699b5ab0f8d969ead9608a67a9d9a21b105faf2
raquel-manajreh <raquel.manajreh@bootcamp-upgrade.com>
2024-07-23 18:29:46 +0200

    primera prueba

--- backend_festival/models/festival.model.js
+++ backend_festival/models/festival.model.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const vehicleSchema = new mongoose.Schema({
-   
+   name: {
+        type: String,
+   },
+   image: {
+        type: String,
+   },
+   description: {
+        type: String,
+   },
+   video: {
+        type: String,
+   }
 });
 
 const Vehicle = mongoose.model('Vehicle', vehicleSchema);
