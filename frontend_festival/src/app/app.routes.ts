e16f17af99e1d24fd84cde9ad654bbea5e93b514
raquel-manajreh <raquel.manajreh@bootcamp-upgrade.com>
2024-07-24 20:18:02 +0200

    sing-up

--- frontend_festival/src/app/app.routes.ts
+++ frontend_festival/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 
 export const routes: Routes = [
+    {
+        path: "signup",
+        component: SignupComponent,
+        canActivate: [isNotLoggedInGuard]
+    }
   
 ];
--- frontend_festival/src/app/pages/signup/signup.component.ts
+++ frontend_festival/src/app/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {  ReactiveFormsModule} from '@angular/forms';
-import {  RouterModule } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,4 +11,29 @@ import {  RouterModule } from '@angular/router';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  passwordType: string = "password"
+
+  form!: FormGroup
+
+  constructor(private builder: FormBuilder,
+     private authService: AuthService,
+     private router: Router){
+    this.form = builder.group({
+      "name": new FormControl(null, [Validators.required]),
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "pwd": new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    })
+  }
+
+  signup() {
+    this.authService.signup(this.form.value.name, this.form.value.email, this.form.value.pwd).subscribe({
+      next: ()=>{
+
+        this.router.navigateByUrl("/login")
+      },
+      error:()=>{
+
+      }
+    })
+  }
 }
